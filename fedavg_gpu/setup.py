1b2f68b5f957d9697f527b894f7767e2732cfa86
Shutong <pengshutong16@gmail.com>
2025-12-06 22:45:35 -0500

    update

--- fedavg_gpu/setup.py
+++ fedavg_gpu/setup.py
@@ -1,19 +1,27 @@
-from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, Extension
+import pybind11, numpy, os
+
+cuda_home = os.environ.get("CUDA_HOME", "/usr/local/cuda")
 
 ext_modules = [
-    Pybind11Extension(
+    Extension(
         "fedavg_gpu",
-        sources=["binding.cpp", "fedavg_gpu.cu"],
-        extra_compile_args={
-            "cxx": ["-O3"],
-            "nvcc": ["-O3"]
-        },
-    ),
+        sources=["binding.cpp"],           
+        extra_objects=["fedavg_gpu.o"],     
+        include_dirs=[
+            pybind11.get_include(),
+            numpy.get_include(),
+            os.path.join(cuda_home, "include"),
+        ],
+        library_dirs=[os.path.join(cuda_home, "lib64")],
+        libraries=["cudart"],
+        language="c++",
+        extra_compile_args=["-O3", "-std=c++14"],
+    )
 ]
 
 setup(
     name="fedavg_gpu",
+    version="0.1",
     ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
 )
