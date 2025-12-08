121393b2d563a47edb2df6e8bb0bcf541a57504a
Shutong <pengshutong16@gmail.com>
2025-12-08 13:33:18 -0500

    Test LSTM with CyberThreat

--- fl_core.py
+++ fl_core.py
@@ -22,8 +22,8 @@ import tensorflow as tf
 # ============================================================
 
 # Switches (server & clients all see the same import)
-MODEL_TYPE   = "cifar_resnet"                       # options: "lstm", "cifar_resnet"
-DATASET_TYPE = "cifar10"              # options: "cyber_threat", "cifar10"
+MODEL_TYPE   = "lstm"                       # options: "lstm", "cifar_resnet"
+DATASET_TYPE = "cyber_threat"              # options: "cyber_threat", "cifar10"
 
 # Text model / dataset parameters (LSTM + cyber-threat)
 MAX_NB_WORDS   = 50000
